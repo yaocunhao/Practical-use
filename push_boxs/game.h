6eacf8df7343671be09f269a2eb73c0b9b78a663
姚存浩 <997106826@qq.com>
2020-12-13 22:24:05 +0800

    推箱子小游戏

---
+++ push_boxs/game.h
@@ -0,0 +1,14 @@
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include <stdio.h>
+#include <windows.h>
+
+
+#define ROW 10
+#define LINE 10
+
+void map(int arr[ROW][LINE]);
+void Game();
+
+#endif
---
+++ push_boxs/man.c
@@ -0,0 +1,17 @@
+#include "game.h"
+
+
+int main()
+{
+
+	
+	
+	
+	
+	
+
+	
+	Game();
+	system("pause");
+	return 0;
+}
